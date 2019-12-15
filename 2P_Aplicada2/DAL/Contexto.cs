5f39dab16c24d75bed9aa9062c2aaed6d0c6e125
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-12-14 22:40:21 -0400

    Clase Contexto

--- 2P_Aplicada2/DAL/Contexto.cs
+++ 2P_Aplicada2/DAL/Contexto.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using _2P_Aplicada2.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace _2P_Aplicada2.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        //public DbSet<Utilidades> Utils { get; set; }
+
+        public Contexto() : base("Constr")
+        {
+        }
     }
 }
